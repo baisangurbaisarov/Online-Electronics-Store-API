071b3f280fe64c536ebc4e5202f2dc4f6e779559
Baisangur <baisangur.baisarov@narxoz.kz>
2026-04-25 10:01:21 +0500

    исправление

--- migrations/000001_create_users.down.sql
+++ migrations/000001_create_users.down.sql
@@ -1,5 +1,1 @@
-CREATE TABLE IF NOT EXISTS users (
-    id         SERIAL PRIMARY KEY,
-    username   VARCHAR(255) NOT NULL UNIQUE,
-    password   VARCHAR(255) NOT NULL
-);
+DROP TABLE IF EXISTS users;
--- migrations/000001_create_users.up.sql
+++ migrations/000001_create_users.up.sql
@@ -1,1 +1,5 @@
-DROP TABLE IF EXISTS users;
+CREATE TABLE IF NOT EXISTS users (
+    id         SERIAL PRIMARY KEY,
+    username   VARCHAR(255) NOT NULL UNIQUE,
+    password   VARCHAR(255) NOT NULL
+);
